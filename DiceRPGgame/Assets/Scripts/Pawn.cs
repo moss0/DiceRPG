e8c41ce0531368a67c30d0295f4307570b82cffd
moss0 <2317919@students.ucreative.ac.uk>
2024-10-17 22:42:19 +0100

    Made an enemy lock on.

--- DiceRPGgame/Assets/Scripts/Pawn.cs
+++ DiceRPGgame/Assets/Scripts/Pawn.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Pawn : MonoBehaviour
 {
-    public float health, damage, defence, speed, hitPeriod;
+    public float health, damage, defence, speed, hitPeriod, minLockOnDist, maxDistanceDelta = 0.005f;
     private float xAxis, yAxis, fixedSpeed;
     public bool isEnemy;
     public string myName;
     public Rigidbody2D myRigidBody;
+    public GameObject player;
     public void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
@@ -19,7 +20,10 @@ public class Pawn : MonoBehaviour
         { 
             ControlMovement(); 
         }
-        
+        if (isEnemy == true)
+        {
+            EnemyMovement();
+        }
     }
 
     public void ControlMovement()
@@ -29,6 +33,24 @@ public class Pawn : MonoBehaviour
         yAxis = Input.GetAxis("Vertical");
         myRigidBody.velocity = new Vector2(xAxis * fixedSpeed, yAxis * fixedSpeed);
     }
+    
+    public void EnemyMovement()
+    {
+        Vector3 vectDiff = player.transform.position - transform.position;
+        fixedSpeed = maxDistanceDelta * Time.deltaTime;
+        if (Vector3.Magnitude(vectDiff) < minLockOnDist)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, maxDistanceDelta);
+        }
+
+        //for (int i = 0; i < 60f * 1/Time.deltaTime; i++)
+        //{
+        //    Debug.Log(Vector3.Magnitude(vectDiff));
+        //    i = 0;
+        //}
+
+    }
+
     public void TakeDamage()
     {
         
